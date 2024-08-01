4985ade87c12a89056e15b8dd16b4ff53cdb8485
kaito <4681kaito@gmail.com>
2024-08-01 20:28:41 +0900

    対戦設定画面

--- public/js/multiSetting.js
+++ public/js/multiSetting.js
@@ -1,19 +1,44 @@
 onload = (_event) => {
 	const selectedOpponent = sessionStorage.getItem("opponent");
-	// 以前の情報があればラジオボタンにチェックを入れる
+	const password = sessionStorage.getItem("password");
+
+	// 以前の情報があればを入れる
 	if (selectedOpponent) {
 		const opponentRadio = document.querySelector(`input[name="Opponent"][value="${selectedOpponent}"]`);
 		if (opponentRadio) {
 			opponentRadio.checked = true;
 		}
 	}
+	if(password){
+		document.querySelector("#password").value = password;
+	}
+	togglePasswordContainer(selectedOpponent || document.querySelector('input[name="Opponent"]:checked').value);
 }
+
 
 document.querySelector("#gameStart").addEventListener("click", function () {
 	const selectedOpponent = document.querySelector('input[name="Opponent"]:checked').value;
-	// セッションストレージにopponentを保存
 	sessionStorage.setItem("opponent", selectedOpponent);
+
+	if(selectedOpponent === "password"){
+		sessionStorage.setItem("password",document.querySelector("#password").value );
+	}
 
 
 	location.href = selectedOpponent + ".html";
 });
+
+document.querySelectorAll('input[name="Opponent"]').forEach(radio => {
+	radio.addEventListener("change", function () {
+		togglePasswordContainer(this.value);
+	});
+});
+
+function togglePasswordContainer(value) {
+	const passwordContainer = document.querySelector("#passwordContainer");
+	if (value === "password") {
+		passwordContainer.style.display = "flex";
+	} else {
+		passwordContainer.style.display = "none";
+	}
+}
